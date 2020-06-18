367ab7389d53f7aeda898fb15b0edd8abce214d0
Zsuzsanna Erdos <ezsana@gmail.com>
2020-06-18 17:35:04 +0200

    Update project according to changes: UniqueProjectpage.

--- src/main/java/com/codecool/zsana/scrumtrackertest/scrumtrackertest/UniqueProjectpage.java
+++ src/main/java/com/codecool/zsana/scrumtrackertest/scrumtrackertest/UniqueProjectpage.java
@@ -7,31 +7,91 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class UniqueProjectpage extends Basepage {
 
-    // Statuses for the freshly created project
+    // Statuses in BaseProjectForTest:
 
-    @FindBy(xpath = "//*[contains(text(),'To Do')]")
+    // First column / container: To Do
+    @FindBy(xpath = "//*[@id='root']//div[@class='scrum_table']/div[@class='project_column'][1]")
     private WebElement todoContainer;
 
-    @FindBy(xpath = "//*[contains(text(),'In Progress')]")
+    // Second column / container: In Progress
+    @FindBy(xpath = "//*[@id='root']//div[@class='scrum_table']/div[@class='project_column'][2]")
     private WebElement inProgressContainer;
 
-    @FindBy(xpath = "//*[contains(text(),'Done')]")
+    // Third column / container: Done
+    @FindBy(xpath = "//*[@id='root']//div[@class='scrum_table']/div[@class='project_column'][3]")
     private WebElement doneContainer;
 
-    @FindBy(xpath = "//div[@class='sc-AxirZ jqONUp add_new_status adder_component']/input[@class='sc-AxiKw hzGrch']")
-    private WebElement addNewStatusInput;
+    @FindBy(xpath = "//*[@id='root']//div[@class='scrum_table']/div[@class='project_column'][1]//div[@class='task_card']/div[contains(text(),'TransferThisTask')]")
+    private WebElement transferThisTaskTitle;
 
-    @FindBy(xpath = "//div[@class='sc-AxirZ jqONUp add_new_task adder_component']/input[@class='sc-AxiKw hzGrch']")
-    private WebElement addNewTaskInput;
+    @FindBy(xpath = "//*[@id='root']//div[@class='scrum_table']/div[@class='project_column'][1]//div[@class='task_card']/div[contains(text(),'EditThisTask')]")
+    private WebElement editThisTaskTitle;
 
-    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div/div[3]/div[1]/span")
+    @FindBy(xpath = "//*[@id='root']//div[@class='scrum_table']/div[@class='project_column'][1]//div[@class='task_card']/div[contains(text(),'ShareThisTask')]")
+    private WebElement shareThisTaskTitle;
+
+    @FindBy(xpath = "//*[@id='root']//div[@class='scrum_table']/div[@class='project_column'][1]//div[@class='task_card']/div[contains(text(),'SendThisTaskByEmail')]")
+    private WebElement sendThisTaskByEmailTitle;
+
+    @FindBy(xpath = "//*[@id='root']//div[@class='scrum_table']/div[@class='project_column'][3]//div[@class='add_new_task']")
+    private WebElement addNewTaskButtonInDoneStatus;
+
+    @FindBy(xpath = "//*[@id='root']//div[@class='scrum_table']/div[@class='project_column'][3]//input[class='add_new_task_text']")
+    private WebElement addNewTaskInputInDoneStatus;
+
+    @FindBy(xpath = "//*[@id='root']//div[@class='scrum_table']/div[@class='project_column'][3]//input[class='add_new_task_btn']")
+    private WebElement addNewTaskSubmitButtonInDoneStatus;
+
+    @FindBy(xpath = "//*[@id='add_status_icon']")
+    private WebElement addNewStatusIcon;
+
+    @FindBy(xpath = "//*[@id='root']//div[@class='scrum_table']/div[4]//input[@class='add_new_column_text']")
+    private WebElement addNewStatusInputField;
+
+    @FindBy(xpath = "//*[@id='root']//div[@class='scrum_table']/div[4]//input[@class='add_new_column_btn']")
     private WebElement addNewStatusSubmitButton;
 
-    @FindBy(xpath = "//*[@id=\"root\"]/div/div/div/div[3]/div[2]/span")
-    private WebElement addNewTaskSubmitButton;
+    @FindBy(xpath = "//*[@id='root']//span[@aria-label='usergroup-add']")
+    private WebElement addNewParticipantButton;
 
-    @FindBy(xpath = "")
-    private WebElement todoTaskDescription;
+    // Pop-up window: add new participant
+    @FindBy(xpath = "//input[@role='combobox']")
+    private WebElement getAddNewParticipantInputField;
+
+    // Pop-up window: add new participant
+    @FindBy(xpath = "//span[@aria-label='user-add']")
+    private WebElement getAddNewParticipantGreenManButton;
+
+    // Pop-up window: add new participant - look for this name after added new participant:
+    @FindBy(xpath = "//div[@class='ant-modal-content']//div[@class='participant_users']//h3[contains(text(),'zsana')]")
+    private WebElement getNewParticipantName;
+
+    // Pop-up window: add new participant
+    @FindBy(xpath = "//div[@class='ant-modal-content']/button[@aria-label='Close']")
+    private WebElement closeAddNewParticipantWindow;
+
+    @FindBy(xpath = "//*[@id='root']//span[@aria-label='mail']")
+    private  WebElement sendEmailButton;
+
+    // Pop-up window: send email
+    @FindBy(xpath = "//div[@class='ant-modal-content']/button[@aria-label='Close']")
+    private WebElement closeSendEmailWindow;
+
+    // Pop-up window: send email
+    @FindBy(xpath = "//div[@class='ant-modal-content']//input[@placeholder='e-mail']")
+    private WebElement sendEmailInputField;
+
+    // Pop-up window: send email
+    @FindBy(xpath = "//div[@class='ant-modal-content']//button[@class='ant-btn']")
+    private WebElement sendEmailPopUpWindowButton;
+
+    // Pop-up window after sending the email
+    @FindBy(xpath = "//span[contains(text(),'Successful e-mail sending')]")
+    private WebElement messageOfSuccessfulEmailSending;
+
+    // Pop-up window after sending the email
+    @FindBy(xpath = "//div[@class='ant-modal-content'][3]//span[@class='ant-modal-close-x']")
+    private WebElement closeMessageOfSuccessfulEmail;
 
 
     public WebElement getTodoContainer() {
@@ -46,30 +106,98 @@ public class UniqueProjectpage extends Basepage {
         return doneContainer;
     }
 
-    public WebElement getAddNewStatusInput() {
-        return addNewStatusInput;
+    public WebElement getTransferThisTaskTitle() {
+        return transferThisTaskTitle;
     }
 
-    public WebElement getAddNewTaskInput() {
-        return addNewTaskInput;
+    public WebElement getEditThisTaskTitle() {
+        return editThisTaskTitle;
+    }
+
+    public WebElement getShareThisTaskTitle() {
+        return shareThisTaskTitle;
+    }
+
+    public WebElement getSendThisTaskByEmailTitle() {
+        return sendThisTaskByEmailTitle;
+    }
+
+    public WebElement getAddNewTaskButtonInDoneStatus() {
+        return addNewTaskButtonInDoneStatus;
+    }
+
+    public WebElement getAddNewTaskInputInDoneStatus() {
+        return addNewTaskInputInDoneStatus;
+    }
+
+    public WebElement getAddNewTaskSubmitButtonInDoneStatus() {
+        return addNewTaskSubmitButtonInDoneStatus;
+    }
+
+    public WebElement getAddNewStatusIcon() {
+        return addNewStatusIcon;
+    }
+
+    public WebElement getAddNewStatusInputField() {
+        return addNewStatusInputField;
     }
 
     public WebElement getAddNewStatusSubmitButton() {
         return addNewStatusSubmitButton;
     }
 
-    public WebElement getAddNewTaskSubmitButton() {
-        return addNewTaskSubmitButton;
+    public WebElement getAddNewParticipantButton() {
+        return addNewParticipantButton;
+    }
+
+    public WebElement getGetAddNewParticipantInputField() {
+        return getAddNewParticipantInputField;
+    }
+
+    public WebElement getGetAddNewParticipantGreenManButton() {
+        return getAddNewParticipantGreenManButton;
+    }
+
+    public WebElement getGetNewParticipantName() {
+        return getNewParticipantName;
+    }
+
+    public WebElement getCloseAddNewParticipantWindow() {
+        return closeAddNewParticipantWindow;
+    }
+
+    public WebElement getSendEmailButton() {
+        return sendEmailButton;
+    }
+
+    public WebElement getCloseSendEmailWindow() {
+        return closeSendEmailWindow;
+    }
+
+    public WebElement getSendEmailInputField() {
+        return sendEmailInputField;
+    }
+
+    public WebElement getSendEmailPopUpWindowButton() {
+        return sendEmailPopUpWindowButton;
+    }
+
+    public WebElement getMessageOfSuccessfulEmailSending() {
+        return messageOfSuccessfulEmailSending;
+    }
+
+    public WebElement getCloseMessageOfSuccessfulEmail() {
+        return closeMessageOfSuccessfulEmail;
     }
 
     public void addNewStatus(String statusName) {
-        writeIntoInputField(addNewStatusInput, statusName);
+        writeIntoInputField(addNewStatusIcon, statusName);
         clickOnElement(addNewStatusSubmitButton);
     }
 
     public void addNewTask(String taskName) {
-        writeIntoInputField(addNewTaskInput, taskName);
-        clickOnElement(addNewTaskSubmitButton);
+        writeIntoInputField(addNewTaskButtonInDoneStatus, taskName);
+        clickOnElement(addNewTaskSubmitButtonInDoneStatus);
     }
 
     public void deleteTask(WebElement task) {
