2ec341d0b981799aea09e8a73f28b177b0db67f4
Zsuzsanna Erdos <ezsana@gmail.com>
2020-07-18 12:50:47 +0200

    Homepage tests rewritten and executed.

--- src/test/java/com/codecool/zsana/scrumtrackertest/HomepageTest.java
+++ src/test/java/com/codecool/zsana/scrumtrackertest/HomepageTest.java
@@ -1,55 +1,50 @@
 package com.codecool.zsana.scrumtrackertest;
 
-import org.junit.jupiter.api.*;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class HomepageTest extends Basetest{
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+public class HomepageTest extends Basetest {
 
     private Homepage homepage;
     private SignIn signIn;
 
-    @BeforeAll
-    void setupTests() {
+    @Given("that I'm logged in")
+    public void setupTest(){
         Basepage.setUp();
         Basepage.goToAppUrl();
         homepage = new Homepage();
         signIn = new SignIn();
+    }
+
+    @When("homepage is loaded")
+    public void signInAndLoadHomepage() {
         signIn.validLoginForTest();
     }
 
-    @AfterAll
-    void closeTests() {
+    @Then("all of the above web elements are seen")
+    @TestFactory
+    public Collection<DynamicTest> elementsAreVisible() {
+        return Arrays.asList(DynamicTest.dynamicTest("Home button", () -> assertTrue(homepage.isElementPresent(homepage.getHomeButton()))),
+                DynamicTest.dynamicTest("Projects button", () -> assertTrue(homepage.isElementPresent(homepage.getProjectsButton()))),
+                DynamicTest.dynamicTest("Logout button", () -> assertTrue(homepage.isElementPresent(homepage.getLogoutButton()))),
+                DynamicTest.dynamicTest("Welcome heading", () -> assertTrue(homepage.isElementPresent(homepage.getWelcomeHeading()))),
+                DynamicTest.dynamicTest("Check Projects link", () -> assertTrue(homepage.isElementPresent(homepage.getCheckProjectsLink()))));
+    }
+
+
+    @And("I close homepage tests.")
+    public void closeTests() {
         signIn.logoutForTest(homepage);
         Basepage.shutDown();
-    }
-
-    /**
-     * Check all elements on homepage are visible
-     */
-
-    @Test
-    void homeButtonIsVisible() {
-        Assertions.assertTrue(homepage.isElementPresent(homepage.getHomeButton()));
-    }
-
-    @Test
-    void projectsButtonIsVisible() {
-        Assertions.assertTrue(homepage.isElementPresent(homepage.getProjectsButton()));
-    }
-
-    @Test
-    void logoutButtonIsVisible() {
-        Assertions.assertTrue(homepage.isElementPresent(homepage.getLogoutButton()));
-    }
-
-    @Test
-    void welcomeMessageIsVisible() {
-        Assertions.assertTrue(homepage.isElementPresent(homepage.getWelcomeHeading()));
-    }
-
-    @Test
-    void projectMessageIsVisible() {
-        Assertions.assertTrue(homepage.isElementPresent(homepage.getCheckProjectsLink()));
     }
 
 }
--- src/test/resources/com.codecool.zsana.scrumtrackertest/homepage.feature
+++ src/test/resources/com.codecool.zsana.scrumtrackertest/homepage.feature
@@ -4,5 +4,5 @@ Feature: Homepage
   Scenario: All web elements seen on homepage: Homepage / Projects / Logout buttons; welcome message; link to projects
 
     Given that I'm logged in
-    And after homepage is loaded
+    When homepage is loaded
     Then all of the above web elements are seen
