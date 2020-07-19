118b7df09d096672697b8e3b1b35c8b72592e364
Zsuzsanna Erdos <ezsana@gmail.com>
2020-07-19 11:05:10 +0200

    Bugfix.

--- src/test/java/com/codecool/zsana/scrumtrackertest/scrumtrackertest/UniqueProjectpageTest.java
+++ src/test/java/com/codecool/zsana/scrumtrackertest/scrumtrackertest/UniqueProjectpageTest.java
@@ -29,7 +29,7 @@ class UniqueProjectpageTest extends Basetest {
         Basepage.goToAppUrl();
         signIn.validLoginForTest();
         homepage.clickOnElement(homepage.getProjectsButton());
-        projectspage.clickOnElement(projectspage.getShareThisProjectTitle());
+        projectspage.clickOnElement(projectspage.getBaseProjectForTestTitle());
     }
 
     @AfterAll
